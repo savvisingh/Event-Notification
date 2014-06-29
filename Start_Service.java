33bc54997efb46fac302ea07cd585d9f4b59e0b3
unknown <Savvi Singh>
2014-06-29 17:14:07 +0530

    commiter

---
+++ Start_Service.java
@@ -0,0 +1,69 @@
+package com.example.sikhi_calender3;
+
+
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.util.Log;
+import android.widget.Toast;
+
+public class Start_Service extends Service{
+	private AlarmManager mAlarmManager;
+	private Intent mNotificationReceiverIntent;
+    private PendingIntent mNotificationReceiverPendingIntent;
+	
+	
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+		mAlarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+		   mNotificationReceiverIntent = new Intent (getApplicationContext(),Broadcast_receiver.class); 
+			mNotificationReceiverPendingIntent = PendingIntent.getBroadcast(
+					this.getApplicationContext(), 0, mNotificationReceiverIntent, 0);
+		
+	}
+
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		if(mNotificationReceiverPendingIntent!=null)
+		{mAlarmManager.cancel(mNotificationReceiverPendingIntent);}
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// TODO Auto-generated method stub
+		
+		
+			
+			
+				mAlarmManager.set(AlarmManager.RTC_WAKEUP,  System.currentTimeMillis() +10000L,
+						 mNotificationReceiverPendingIntent);
+				Toast.makeText(getApplicationContext(), " Alarm Set",
+						Toast.LENGTH_LONG).show();
+			
+		
+		
+		
+		Log.i("Alarm","Set");
+		
+		return START_NOT_STICKY;
+		
+		
+		
+	}
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
